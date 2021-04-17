f2728c2fe777dc805b263170627f3d47008173d0
yourh3ro <nornikbiz@gmail.com>
2021-04-18 02:27:59 +0700

    add some words to words.py

--- words.py
+++ words.py
@@ -1,7 +1,7 @@
 import random
 
 def random_minus_words():
-    randint = random.randint(1,5)
+    randint = random.randint(1,9)
 
     if randint == 1:
         rand_minus_words_result = ' залил соляры '
@@ -13,11 +13,19 @@ def random_minus_words():
         rand_minus_words_result = ' негодует от '
     elif randint == 5:
         rand_minus_words_result = ' минусанул '
+    elif randint == 6:
+        rand_minus_words_result = ' попустил '
+    elif randint == 7:
+        rand_minus_words_result = ' облил мочой '
+    elif randint == 8:
+        rand_minus_words_result = ' заговнил '
+    elif randint == 9:
+        rand_minus_words_result = ' потянул на дно '
 
     return rand_minus_words_result
 
 def random_plus_words():
-    randint = random.randint(1,5)
+    randint = random.randint(1,8)
 
     if randint == 1:
         rand_plus_words_result = ' залил рофлан '
@@ -29,5 +37,11 @@ def random_plus_words():
         rand_plus_words_result = ' одобряет '
     elif randint == 5:
         rand_plus_words_result = ' плюсанул '
+    elif randint == 6:
+        rand_plus_words_result = ' позитивнул '
+    elif randint == 7:
+        rand_plus_words_result = ' поддержал '
+    elif randint == 8:
+        rand_plus_words_result = ' законтачил с '
 
     return rand_plus_words_result
